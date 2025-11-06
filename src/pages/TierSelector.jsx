10f422699647627a3f089f91f64fa9b1b576ccec
victornavareno <vnavareo@alumnos.unex.es>
2025-11-06 19:49:19 +0100

    feat- mocked tierSelector.jsx

--- src/pages/TierSelector.jsx
+++ src/pages/TierSelector.jsx
@@ -109,7 +109,7 @@ function TierSelector() {
             <div className="container mx-auto px-6 pt-48 pb-24"> {/* Ajuste de padding superior */}
                 <div className="text-center max-w-3xl mx-auto mb-16">
                     <h1 className="text-5xl md:text-6xl font-extrabold tracking-tight mb-4">
-                        Planes pensados para <span className='bg-gradient-to-r from-purple-500 to-pink-500 bg-clip-text text-transparent'>ti</span>
+                        Elige <span className="bg-gradient-to-r from-[#e32eff] to-[#ec008c] bg-clip-text text-transparent">el mejor plan</span> en tu local
                     </h1>
                     <p className="text-xl text-gray-400">
                         Elige el nivel que mejor se adapta a tu negocio y empieza a conectar con miles de usuarios en Cáceres.
